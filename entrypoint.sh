ed277300a0adf9a142b64dcc7f11bcfc468264d8
Chris Fung <aergonaut@gmail.com>
2019-09-29 23:31:16 -0700

    change output location

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cd $GITHUB_WORKSPACE
 bundle install --jobs 4 --quiet
 
 set +e
-bundle exec srb tc 2> /srb-tc-output.txt
+bundle exec srb tc 2> ./srb-tc-output.txt
 
 set -e
-ruby /parse_results.rb /src-tc-output.txt
+ruby /parse_results.rb ./src-tc-output.txt
