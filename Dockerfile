9eae5b8684268125c11a1f4b838c924f51afce96
Chris Fung <aergonaut@gmail.com>
2019-09-29 22:33:15 -0700

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ruby:2.6
 
 COPY entrypoint.sh /entrypoint.sh
+COPY parse_results.rb /parse_results.rb
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
+cd $GITHUB_WORKSPACE
 bundle install --jobs 4 --quiet
 bundle exec srb tc > /srb-tc-output.txt
 
-ruby parse_results.rb /src-tc-output.txt
+ruby /parse_results.rb /src-tc-output.txt
